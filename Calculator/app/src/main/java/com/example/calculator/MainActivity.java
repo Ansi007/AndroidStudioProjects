882dd4da1e93c2ee102b35810caaef46bd96168e
Anns <Anns@DESKTOP-PQMHB1A>
2022-04-28 09:03:05 +0500

    Binded

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button btnClear, btnEqual, btnOne, btnTwo,btnThree,btnFour,btnFive,btnSix,btnSeven,btnEight,btnNine;
+    Button btnMul,btnMod,btnDiv,btnAdd,btnSub;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnClear = findViewById(R.id.btnClear);
+        btnEqual = findViewById(R.id.btnEqual);
+        btnOne = findViewById(R.id.btnTwo);
+        btnTwo = findViewById(R.id.btnClear);
+        btnThree = findViewById(R.id.btnThree);
+        btnFour = findViewById(R.id.btnFour);
+        btnFive = findViewById(R.id.btnFive);
+        btnSix = findViewById(R.id.btnSix);
+        btnSeven = findViewById(R.id.btnSeven);
+        btnEight = findViewById(R.id.btnEight);
+        btnMul = findViewById(R.id.btnMul);
+        btnMod = findViewById(R.id.btnMod);
+        btnDiv = findViewById(R.id.btnDiv);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnSub = findViewById(R.id.btnSub);
+
+    }
+
+    @Override
+    public void onClick(View v){
+
     }
 }
