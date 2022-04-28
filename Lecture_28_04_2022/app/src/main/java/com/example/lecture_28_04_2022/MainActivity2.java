6827b46e8d81fe995a9f517cb21efe83a46f8488
Anns <Anns@DESKTOP-PQMHB1A>
2022-04-28 08:45:05 +0500

    Lecture 28/4/2022

---
+++ Lecture_28_04_2022/app/src/main/java/com/example/lecture_28_04_2022/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.lecture_28_04_2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
