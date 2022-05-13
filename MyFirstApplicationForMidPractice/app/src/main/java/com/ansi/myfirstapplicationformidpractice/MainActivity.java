0d830258224160b3ecc2d14fb3cc9e7c2db8dc3f
Anns <Anns@DESKTOP-PQMHB1A>
2022-05-13 05:54:11 +0500

    Mids Practice Constraint Layout

---
+++ MyFirstApplicationForMidPractice/app/src/main/java/com/ansi/myfirstapplicationformidpractice/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ansi.myfirstapplicationformidpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_main);
+    }
+
+}
