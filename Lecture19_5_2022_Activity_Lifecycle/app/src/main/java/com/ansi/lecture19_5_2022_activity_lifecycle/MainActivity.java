57d7635368103acfc3a72a20e4a8c1ff33477000
Anns <Anns@DESKTOP-PQMHB1A>
2022-05-19 20:23:02 +0500

    New Image Resources added for test module

--- Lecture19_5_2022_Activity_Lifecycle/app/src/main/java/com/ansi/lecture19_5_2022_activity_lifecycle/MainActivity.java
+++ Lecture19_5_2022_Activity_Lifecycle/app/src/main/java/com/ansi/lecture19_5_2022_activity_lifecycle/MainActivity.java
@@ -2,13 +2,76 @@ package com.ansi.lecture19_5_2022_activity_lifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Tag","MainActivityOnCreate");
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("value", "Last Button Text");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String myString = savedInstanceState.getString("value");
+        btn.setText(myString);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("Tag","MainActivityOnStart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("Tag","MainActivityOnResume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("Tag","MainActivityOnPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("Tag","MainActivityOnStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("Tag","MainActivityOnDestroy");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("Tag","MainActivityOnRestart");
     }
 }
--- Lecture19_5_2022_Activity_Lifecycle/app/src/main/java/com/ansi/lecture19_5_2022_activity_lifecycle/SecondActivity.java
+++ Lecture19_5_2022_Activity_Lifecycle/app/src/main/java/com/ansi/lecture19_5_2022_activity_lifecycle/SecondActivity.java
@@ -3,6 +3,7 @@ package com.ansi.lecture19_5_2022_activity_lifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,42 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Log.d("Tag2","SecondActivityOnCreate");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("Tag2","SecondActivityOnStart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("Tag2","SecondActivityOnResume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("Tag2","SecondActivityOnPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("Tag2","SecondActivityOnStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("Tag2","SecondActivityOnDestroy");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("Tag2","SecondActivityOnRestart");
     }
 }
