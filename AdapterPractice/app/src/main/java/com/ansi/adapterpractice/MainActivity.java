7922d888656b4a776c2e59a4e9577defaf183012
Anns <Anns@DESKTOP-PQMHB1A>
2022-05-24 15:22:49 +0500

    AdapterPractice XML Completed

--- AdapterPractice/app/src/main/java/com/ansi/adapterpractice/MainActivity.java
+++ AdapterPractice/app/src/main/java/com/ansi/adapterpractice/MainActivity.java
@@ -3,6 +3,9 @@ package com.ansi.adapterpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
