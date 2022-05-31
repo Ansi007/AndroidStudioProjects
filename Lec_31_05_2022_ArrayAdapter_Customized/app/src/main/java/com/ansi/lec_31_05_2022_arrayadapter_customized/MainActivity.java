3b1d88a0ce3a8e2ba5aa8e9f6bf1e48a899e9ae6
Anns Shahbaz <annsshahbaz@gmail.com>
2022-05-31 08:34:19 +0500

    Lecture_31_05_2022_Customized_List_View

---
+++ Lec_31_05_2022_ArrayAdapter_Customized/app/src/main/java/com/ansi/lec_31_05_2022_arrayadapter_customized/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ansi.lec_31_05_2022_arrayadapter_customized;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
