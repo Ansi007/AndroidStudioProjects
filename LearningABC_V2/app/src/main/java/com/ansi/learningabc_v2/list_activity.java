05886e90f5c5df87e8fc862067a4c359a0f75dca
Anns Shahbaz <annsshahbaz@gmail.com>
2022-05-31 20:21:27 +0500

    Alphabets added in Lists

--- LearningABC_V2/app/src/main/java/com/ansi/learningabc_v2/list_activity.java
+++ LearningABC_V2/app/src/main/java/com/ansi/learningabc_v2/list_activity.java
@@ -3,12 +3,21 @@ package com.ansi.learningabc_v2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class list_activity extends AppCompatActivity {
-
+    ListView mainListView;
+    ArrayList<AlphabetWithImage> myImageList = new ArrayList<AlphabetWithImage>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        mainListView = findViewById(R.id.mainList);
+        myImageList.add(new AlphabetWithImage(R.drawable.apple,"Aa"));
+        myImageList.add(new AlphabetWithImage(R.drawable.airplane,"Aa"));
+        MyAdapter myAdapter = new MyAdapter(this,0,myImageList);
+        mainListView.setAdapter(myAdapter);
     }
 }
