d86f81bdfd4a30629cd379678f162aa9e2ad8ca0
Anns Shahbaz <annsshahbaz@gmail.com>
2022-05-31 20:00:52 +0500

    Added: MyAdapter.Java

--- LearningABC_V2/app/src/main/java/com/ansi/learningabc_v2/MyAdapter.java
+++ LearningABC_V2/app/src/main/java/com/ansi/learningabc_v2/MyAdapter.java
@@ -1,5 +1,33 @@
 package com.ansi.learningabc_v2;
 
-public class MyAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class MyAdapter extends ArrayAdapter<AlphabetWithImage> {
+
+    public MyAdapter(@NonNull Context context, int resource, @NonNull List<AlphabetWithImage> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        AlphabetWithImage alpha = getItem(position);
+        convertView = LayoutInflater.from(getContext()).inflate(R.layout.letterwithimage,parent,false);
+        ImageView image = convertView.findViewById(R.id.imageViewShape);
+        image.setImageResource(alpha.getImageId());
+        TextView alphabetTextView = convertView.findViewById(R.id.textViewLetter);
+        alphabetTextView.setText(alpha.getAlphabet());
+        return convertView;
+    }
 }
